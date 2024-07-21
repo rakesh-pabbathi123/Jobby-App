2206356983e963fb7b426254e5f20bb64215b580
rakesh-pabbathi123 <rakeshpabbathi15@gmail.com>
2024-07-21 09:41:48 +0000

    first commit

---
+++ src/components/Login/index.js
@@ -0,0 +1,119 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+import {Redirect} from 'react-router-dom'
+
+import './index.css'
+
+class Login extends Component {
+  state = {
+    username: '',
+    password: '',
+    errorMsg: '',
+    showErrorMsg: false,
+  }
+
+  onSuccessLogin = jwtToken => {
+    Cookies.set('jwt_token', jwtToken, {expires: 30})
+    const {history} = this.props
+    history.replace('/')
+  }
+
+  onFailureLogin = errorMsg => {
+    this.setState({errorMsg, showErrorMsg: true})
+  }
+
+  onSubmitForm = async event => {
+    event.preventDefault()
+    let {username, password} = this.state
+
+    if (username.toLowerCase().trim() === 'rakesh') username = 'rahul'
+    if (password === 'Rakhi@123') password = 'rahul@2021'
+
+    const userDetails = {username, password}
+    const loginApiUrl = 'https://apis.ccbp.in/login'
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(userDetails),
+    }
+    const response = await fetch(loginApiUrl, options)
+    const data = await response.json()
+
+    if (response.ok) {
+      this.onSuccessLogin(data.jwt_token)
+    } else {
+      this.onFailureLogin(data.error_msg)
+    }
+  }
+
+  updateUsername = event => this.setState({username: event.target.value})
+
+  updatePassword = event => this.setState({password: event.target.value})
+
+  renderUsernameField = () => {
+    const {username} = this.state
+    return (
+      <div className="input-field-container">
+        <label htmlFor="username" className="login-input-label">
+          USERNAME
+        </label>
+        <input
+          type="text"
+          value={username}
+          className="login-input-field"
+          placeholder="Rakesh"
+          id="username"
+          onChange={this.updateUsername}
+        />
+      </div>
+    )
+  }
+
+  renderPasswordField = () => {
+    const {password} = this.state
+    return (
+      <div className="input-field-container">
+        <label htmlFor="password" className="login-input-label">
+          PASSWORD
+        </label>
+        <input
+          type="password"
+          value={password}
+          className="login-input-field"
+          placeholder="Rakhi@123"
+          id="password"
+          onChange={this.updatePassword}
+        />
+      </div>
+    )
+  }
+
+  render() {
+    const jwtToken = Cookies.get('jwt_token')
+    if (jwtToken !== undefined) {
+      return <Redirect to="/" />
+    }
+    const {errorMsg, showErrorMsg} = this.state
+    return (
+      <div className="login-container">
+        <form className="login-form" onSubmit={this.onSubmitForm}>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+            alt="website logo"
+            className="website-logo-login-form"
+          />
+          {this.renderUsernameField()}
+          {this.renderPasswordField()}
+          <div>
+            <button type="submit" className="login-button">
+              Login
+            </button>
+            {showErrorMsg && <p className="error-msg">*{errorMsg}</p>}
+          </div>
+        </form>
+        <h1 className="creator-info">Created By: Rakesh Pabbathi</h1>
+      </div>
+    )
+  }
+}
+
+export default Login
